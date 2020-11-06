cf1e0bd0eac19d180898e6071c89f09f069928fa
Victor Salajan <victorsalajan29@gmail.com>
2020-11-06 21:43:44 +0200

    Creted Change_week views and updated weekly template

--- SchedulingApp/appointments/urls.py
+++ SchedulingApp/appointments/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+#from django.contrib.auth.decorators import login_required
+#from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', login_required(TemplateView.as_view(template_name="choose_day.html"))),
+#    path('', login_required(TemplateView.as_view(template_name="choose_day.html"))),
     path('', views.Change_day.homepage, name='homepage'),
-    path('weekly', views.weekly, name='weekly'),
+    path('weekly', views.Change_week.weekly, name='weekly'),
     path('next_day', views.Change_day.next_day, name='next_day'),
-    path('previous_day', views.Change_day.previous_day, name='previous_day')
+    path('previous_day', views.Change_day.previous_day, name='previous_day'),
+    path('previous_week', views.Change_week.previous_week, name='previous_week'),
+    path('next_week', views.Change_week.next_week, name='next_week'),
+    path('register', views.register, name='register')
 ]
--- SchedulingApp/appointments/views.py
+++ SchedulingApp/appointments/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from appointments.models import Clients, Appointments
 from datetime import datetime, date, timedelta, time
 import calendar
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
+def register(response):
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('')
+    else:
+        form = UserCreationForm()
+
+    return render(response, 'register.html', {"form": form})
+
+def get_query_set_and_intervals(query_set):
+    """ For both daily & weekly views """
+    query_set_and_intervals = []
+    for _, obj in enumerate(query_set):
+        start = obj.datetime.time()
+        end = datetime.combine(
+                        date.today(), 
+                        time(obj.datetime.time().hour, obj.datetime.time().minute)
+                        ) + timedelta(minutes=50)
+        end = end.time()
+        query_set_and_intervals.append([obj, f'{str(start)[:5]} - {str(end)[:5]}'])
+    return query_set_and_intervals
 
 class Change_day:
     current_date = datetime.now()
@@ -53,42 +78,60 @@ class Change_day:
         context = Change_day.query_set_and_context()
         return render(request, 'choose_day.html', context)
 
-def get_query_set_and_intervals(query_set):
-    query_set_and_intervals = []
-    for _, obj in enumerate(query_set):
-        start = obj.datetime.time()
-        end = datetime.combine(
-                        date.today(), 
-                        time(obj.datetime.time().hour, obj.datetime.time().minute)
-                        ) + timedelta(minutes=50)
-        end = end.time()
-        query_set_and_intervals.append([obj, f'{str(start)[:5]} - {str(end)[:5]}'])
-    return query_set_and_intervals
-
-@login_required
-def weekly(request):
-    """ Currently not adapted for weeks that belong to two years """
+class Change_week:
     now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    this_week_nr = date(year, month, day).isocalendar()[1]
-    query_set = Appointments.objects.filter(datetime__week=this_week_nr)
-    query_set = query_set.order_by('datetime')
 
-    while now.isocalendar()[2]  > 1:
-        now -= timedelta(days=1)
+    @staticmethod
+    def query_set_and_context(now):
+        current_week = date(now.year, now.month, now.day).isocalendar()[1]
+        query_set = Appointments.objects.filter(datetime__week=current_week)
+        query_set = query_set.order_by('datetime')
 
-    day_labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    dates = []
-    for i in range(5):
-        dates.append([day_labels[i], calendar.month_name[now.month][:3], now.day])
-        now += timedelta(days=1)
-    
-    query_set_and_intervals = get_query_set_and_intervals(query_set)
+        while now.isocalendar()[2]  > 1:
+            now -= timedelta(days=1)
 
-    context = {
-        "WeeklyAppointments": query_set_and_intervals,#query_set,
-        "dates": dates,
-    }
-    return render(request, 'weekly.html', context)
+        day_labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        
+        no_appointments = []
+        for i in range(5):
+            if len(query_set.filter(datetime__day=now.day, datetime__month=now.month, datetime__year=now.year)) == 0:
+                no_appointments.append(True)
+            else:
+                no_appointments.append(False)
+        
+        dates = []
+        for i in range(5):
+            dates.append([day_labels[i], calendar.month_name[now.month][:3], now.day])
+            now += timedelta(days=1)
+
+        query_set_and_intervals = get_query_set_and_intervals(query_set)
+
+        context = {
+            "WeeklyAppointments": query_set_and_intervals,
+            "dates": dates,
+            "no_appointments": no_appointments
+        }
+        return context
+
+    @staticmethod
+    def weekly(request):
+        """ Currently not adapted for weeks that belong to two years """
+        Change_week.now = datetime.now()
+        now = Change_week.now
+        context = Change_week.query_set_and_context(now)
+
+        return render(request, 'weekly.html', context)
+
+    @staticmethod
+    def previous_week(request):
+        Change_week.now -= timedelta(days=7)
+        context = Change_week.query_set_and_context(Change_week.now)
+        
+        return render(request, 'weekly.html', context)
+
+    @staticmethod
+    def next_week(request):
+        Change_week.now += timedelta(days=7)
+        context = Change_week.query_set_and_context(Change_week.now)
+        
+        return render(request, 'weekly.html', context)
